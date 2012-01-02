298b829d0c679733e955b55492a7299e70941454
BobZhome <BobZhome@Cheerful.com>
2012-01-02 12:35:39 -0500

    qdsp6: Allow audio_rx_path_id to be zero, fixes Bluetooth This patch removes the audio_rx_path_id check and now BT is fine again.
    Conflicts:
    
    	arch/arm/mach-msm/qdsp6/q6audio.c

--- arch/arm/mach-msm/qdsp6/q6audio.c
+++ arch/arm/mach-msm/qdsp6/q6audio.c
@@ -1054,7 +1054,8 @@ static void adie_rx_path_enable(uint32_t acdb_id)
 	adie_set_path(adie, audio_rx_path_id, ADIE_PATH_RX);
 	adie_set_path_freq_plan(adie, ADIE_PATH_RX, 48000);
 
-	adie_proceed_to_stage(adie, ADIE_PATH_RX, ADIE_STAGE_DIGITAL_READY);
+	adie_proceed_to_stage(adie, ADIE_PATH_RX,
+			ADIE_STAGE_DIGITAL_READY);
 	adie_proceed_to_stage(adie, ADIE_PATH_RX,
 			ADIE_STAGE_DIGITAL_ANALOG_READY);
 }
