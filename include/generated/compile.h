483698f6fc130c5ff8ca50a28f38c8f822064f61
BobZhome <BobZhome@Cheerful.com>
2012-02-18 21:15:59 -0500

    Initial import of LS670 2.6.35.7

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Fri Aug 19 16:25:00 IST 2011"
+#define LINUX_COMPILE_TIME "16:25:00"
+#define LINUX_COMPILE_BY "lg-electronics"
+#define LINUX_COMPILE_HOST "si-rd10-BLD165"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
