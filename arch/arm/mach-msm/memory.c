de290a1c6481bf0912ee6b1538499fd6a0be9ce5
BobZhome <BobZhome@Cheerful.com>
2012-02-22 20:17:33 -0500

    Add genlock and msm related changes

--- arch/arm/mach-msm/memory.c
+++ arch/arm/mach-msm/memory.c
@@ -17,12 +17,12 @@
 #include <linux/mm.h>
 #include <linux/mm_types.h>
 #include <linux/bootmem.h>
-#include <linux/module.h>
+#include <linux/memory_alloc.h>
 #include <asm/pgtable.h>
 #include <asm/io.h>
 #include <asm/mach/map.h>
 #include <asm/cacheflush.h>
-#include <linux/hardirq.h>
+#include <mach/msm_memtypes.h>
 #if defined(CONFIG_MSM_NPA_REMOTE)
 #include "npa_remote.h"
 #include <linux/completion.h>
@@ -120,6 +120,14 @@ void clean_caches(unsigned long vstart,
 
 	flush_axi_bus_buffer();
 }
+
+unsigned long allocate_contiguous_ebi_nomap(unsigned long size,
+	unsigned long align)
+{
+	return _allocate_contiguous_memory_nomap(size, MEMTYPE_EBI0,
+		align, __builtin_return_address(0));
+}
+EXPORT_SYMBOL(allocate_contiguous_ebi_nomap);
 
 void invalidate_caches(unsigned long vstart,
 	unsigned long length, unsigned long pstart)
